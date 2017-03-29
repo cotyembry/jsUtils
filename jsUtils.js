ff275eb18f3c8004f3637a31c17a1f0bca430594
cotyembry <cotyembry@gmail.com>
2017-03-29 15:34:57 -0500

    Update jsUtils.js

--- jsUtils.js
+++ jsUtils.js
@@ -1,4 +1,27 @@
 var jsUtil = {
+  getScrollBarWidth: function() {
+	var inner = document.createElement('p');
+	inner.style.width = '100%';
+	inner.style.height = '200px';
+	var outer = document.createElement('div');
+	outer.style.position = 'absolute';
+	outer.style.top = '0px';
+	outer.style.left = '0px';
+	outer.style.visibility = 'hidden';
+	outer.style.width = '200px';
+	outer.style.height = '150px';
+	outer.style.overflow = "hidden";
+	outer.appendChild(inner);
+	document.body.appendChild(outer);
+	var w1 = inner.offsetWidth;
+	outer.style.overflow = 'scroll';
+	var w2 = inner.offsetWidth;
+	if (w1 == w2) w2 = outer.clientWidth;
+	document.body.removeChild(outer);
+	  
+	return (w1 - w2);
+  },
+	
   //hasHorizontalBar will return true or false depending on the answer
   hasHorizontalBar: function(DOMElementToCheck) {
 	//this checks the element to see if a scrollbar is present
@@ -12,6 +35,7 @@ var jsUtil = {
 	return hasHorizontalScrollbar;
   }
 }
+
 
 //Just add the code to be able to inherit here, and import the variable in the code to use and bam, there it is
 Array.prototype.insert = function (index, item) {
